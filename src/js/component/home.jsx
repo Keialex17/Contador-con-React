7c76b1d2676f13175ba87cc4ce686c8f3e722d0b
Keialex17 <hernandezkeiber34@gmail.com>
2022-11-29 00:14:43 +0000

    casi

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({counter, one, two, three, four, five, six }) => {
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<Fragment>
+		<div className="container">
+		<div className="row">
+		<div className="col-12 d-flex flex-row-reverse">
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{one % 10}</div>
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (two % 10)}</div>
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (three % 10)}</div>
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (four % 10)}</div>
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (five % 10)}</div>
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (six % 10)}</div>
 		</div>
+		</div>
+		</div>
+		</Fragment>
 	);
 };
 
