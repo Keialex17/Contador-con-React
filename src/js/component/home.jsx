59d0cf86fa17e31a49c71430ab7281f59931884b
Keialex17 <hernandezkeiber34@gmail.com>
2023-01-08 19:21:25 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,9 +8,11 @@ const Home = ({counter, one, two, three, four, five, six }) => {
 
 	return (
 		<Fragment>
-		<div className="container">
+		<div className="container d-flex align-self-center">
 		<div className="row">
 		<div className="col-12 d-flex flex-row-reverse">
+		<script src="https://kit.fontawesome.com/cda3887f65.js" crossorigin="anonymous"></script>
+			<div className="p-5 display-l bg-dark text-white border border-white fs-1"> <div><i className="far fa-clock"></i></div> </div>
 			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{one % 10}</div>
 			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (two % 10)}</div>
 			<div className="p-5 display-l bg-dark text-white border border-white fs-1">{Math.floor (three % 10)}</div>
